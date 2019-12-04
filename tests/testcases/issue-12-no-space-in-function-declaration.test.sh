a5c0e728cf0becf5563d1c10a214ed027309ab89
Egor Kovetskiy <e.kovetskiy@gmail.com>
2019-12-04 21:44:52 +0300

    Fix missing space after one-line descriptions
    Also add test to ensure fix #12

---
+++ tests/testcases/issue-12-no-space-in-function-declaration.test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+tests:put input <<EOF
+#!/bin/bash
+
+# @description a desc for fn1
+fn1() {
+ return 0
+}
+
+# @description a desc for fn2
+fn2(){
+ return 0
+}
+EOF
+
+tests:put expected <<EOF
+
+* [fn1()](#fn1)
+* [fn2()](#fn2)
+
+
+## fn1()
+
+a desc for fn1
+
+## fn2()
+
+a desc for fn2
+EOF
+
+assert
