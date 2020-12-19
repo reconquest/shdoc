b5402c28ac9cfbb3f6c44c76e176b2ef8a12167b
Egor Kovetskiy <e.kovetskiy@gmail.com>
2020-12-19 12:50:49 +0300

    add the missing trailing newline in See also. Fix #25

---
+++ tests/testcases/issue-25-missing-trailing-newline-in-see-also.test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+tests:put input <<EOF
+#!/bin/bash
+
+# @description a desc for fn1
+# @see fn2()
+fn1() {
+ return 0
+}
+
+# @description a desc for fn2
+fn2(){
+ return 0
+}
+EOF
+
+tests:put expected <<EOF
+## Index
+
+* [fn1()](#fn1)
+* [fn2()](#fn2)
+
+### fn1()
+
+a desc for fn1
+
+#### See also
+
+* [fn2()](#fn2)
+
+### fn2()
+
+a desc for fn2
+EOF
+
+assert
