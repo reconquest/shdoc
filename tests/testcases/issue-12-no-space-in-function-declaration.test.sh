5fefc4cef0c355bff34e18cf710f484908d98a37
yanagiragi <qaz90473@gmail.com>
2022-04-08 11:58:50 +0800

    test: Update testcases

--- tests/testcases/issue-12-no-space-in-function-declaration.test.sh
+++ tests/testcases/issue-12-no-space-in-function-declaration.test.sh
@@ -17,14 +17,14 @@ EOF
 tests:put expected <<EOF
 ## Index
 
-* [fn1()](#fn1)
-* [fn2()](#fn2)
+* [fn1](#fn1)
+* [fn2](#fn2)
 
-### fn1()
+### fn1
 
 a desc for fn1
 
-### fn2()
+### fn2
 
 a desc for fn2
 EOF
