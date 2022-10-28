23ad1e98e2800fc6f40285480552303e0a39e672
Pierre-Yves Landuré <pierre-yves@landure.fr>
2022-10-28 06:33:26 +0200

    Add multple line stderr message functionnality, and some tests.

---
+++ tests/testcases/@stderr.test.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# @file test/testcases/@stderr.test.sh
+# @author Pierre-Yves Landuré < contact at biapy dot fr >
+# @brief Test cases for @stderr keyword.
+# @description
+#   Test these @stderr comportements:
+#   - one string (i.e. function name.)
+#   - two strings
+#   - function name containing ':'.
+#   - string starting by '.'
+#   - one relative path starting with './'.
+#   - one relative path starting with '../'.
+#   - one absolute path.
+#   - string that is not a relative path (starting with '.../').
+#   - a https URL.
+#   - a file URL.
+#   - a URL in a text.
+#   - two URL in a text.
+#   - a markdown link.
+#   - a text containing a markdown link.
+#   - a text containing a markdown link and a URL.
+
+tests:put input <<EOF
+# @name shdoc @stderr tests
+# @brief Test @stderr functionnality.
+# @description Tests for shdoc processing of @stderr keyword.
+# @description test-stderr dummy function.
+# @stderr Standard stderr message.
+# @stdout section should appear before stderr section.
+# @see see-also-after-stderr-section
+# @stderr Stderr message with [markdown link](https://github.com/reconquest/shdoc).
+# @stderr       Indented with spaces stderr message.
+# @stderr Multiple lines
+#   stderr message.
+# @stderr Stderr message with trailing spaces.      
+test-stderr() {
+}
+EOF
+
+tests:put expected <<EOF
+Tests for shdoc processing of @stderr keyword.
+## Index
+
+* [test-stderr](#test-stderr)
+
+### test-stderr
+
+test-stderr dummy function.
+
+#### Output on stdout
+
+* section should appear before stderr section.
+
+#### Output on stderr
+
+* Standard stderr message.
+* Stderr message with [markdown link](https://github.com/reconquest/shdoc).
+*       Indented with spaces stderr message.
+* Multiple lines
+* Stderr message with trailing spaces.    
+
+#### See also
+
+* [see-also-after-stderr-section](#see-also-after-stderr-section)
+
+EOF
+
+assert
